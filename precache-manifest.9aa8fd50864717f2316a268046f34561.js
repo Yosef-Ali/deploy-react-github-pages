6a23f241fa026d2d7e8e6df76befdfe4542d0b06
Yosef Ali <yosethio@yahoo.com>
2019-01-31 17:13:40 +0300

    Updates

---
+++ precache-manifest.9aa8fd50864717f2316a268046f34561.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "210935bd632b0f3ae5b1",
+    "url": "/deploy-react-github-pages/static/js/runtime~main.210935bd.js"
+  },
+  {
+    "revision": "83839318f59403041af3",
+    "url": "/deploy-react-github-pages/static/js/main.83839318.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/deploy-react-github-pages/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "83839318f59403041af3",
+    "url": "/deploy-react-github-pages/static/css/main.19b505b3.chunk.css"
+  },
+  {
+    "revision": "3d3ff126dfb4338a8dd626310262859e",
+    "url": "/deploy-react-github-pages/index.html"
+  }
+];
